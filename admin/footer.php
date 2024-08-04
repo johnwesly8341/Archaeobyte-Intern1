574dc22da87fba6d12127e7f2d6dc6878ebeef3b
wesly <vempatijohnwesly6@gmail.com>
2024-08-04 12:17:14 +0530

    groceryweb commit

---
+++ admin/footer.php
@@ -0,0 +1,3 @@
+<footer>
+<p>&copy 2019 . All Rights Reserved  </p>
+</footer>
---
+++ dbconfig.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+DEFINE("server","localhost");
+DEFINE("user","root");
+DEFINE("password","");
+DEFINE("database","online");
+
+	function iud($query)
+	{
+		$cid=mysqli_connect(server,user,password,database) or die("connection error");
+	$result=mysqli_query($cid,$query);
+	$n=mysqli_affected_rows($cid);
+	mysqli_close($cid);
+	return $n;
+	}
+	
+function select($query)
+{
+	$cid=mysqli_connect(server,user,password,database) or die("connection error");
+	$result=mysqli_query($cid,$query);
+	mysqli_close($cid);
+	return $result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+	
